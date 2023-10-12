0afa1e2bea2e4e843e07d2e34ac00066feb21c26
jcovington16 <covingtonjoshua7@gmail.com>
2023-10-12 11:01:34 -0600

    chore: added crate for user interaction.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use serde::{Deserialize, Serialize};
+use dialoguer::Select;
+
 // start off with seeing if we need to grab a password or store one
 // if we need to store one
     // check to see if the password file is made if not make it
@@ -11,5 +13,13 @@ use serde::{Deserialize, Serialize};
 
 
 fn main() {
-    println!("Hello, world!");
+    let choices = vec!["New Password", "Grab Password", "Exit"];
+    
+    let selection = Select::new()
+    .with_prompt("Select an option.")
+    .items(&choices)
+    .interact()
+    .unwrap();
+
+    println!("You chose: {}", choices[selection]);
 }
