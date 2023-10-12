3f93b7aa858484deff24c7b9546d37b0b59ac677
jcovington16 <covingtonjoshua7@gmail.com>
2023-10-12 09:53:10 -0600

    chore: first commit. initializing code.

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use serde::{Deserialize, Serialize};
+// start off with seeing if we need to grab a password or store one
+// if we need to store one
+    // check to see if the password file is made if not make it
+    // create an object with the url as the key
+        // ensure that the object contains username and password with thier values
+            // ensure the password is encrypted.
+// if we need to pull a password
+    // ask for the url
+        // use that url to grab the object from the file and show the object
+
+
+fn main() {
+    println!("Hello, world!");
+}
